8e62d5300d3e38f2522e5c1a7dab9195b8e36491
Saidabdi38 <ibnucabdi@hotmail.com>
2026-02-12 19:11:16 +0300

    Update URL

--- exam_site/urls.py
+++ exam_site/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("", include("exams.urls")),
-    path("", include("exams.teacher_urls")),
+    path("teacher/", include("exams.teacher_urls")),
 
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
     path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
